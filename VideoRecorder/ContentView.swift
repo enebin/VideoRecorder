7b35e85102a39cdd375922394afa0fecb4b66047
enebin <enebin777@gmail.com>
2023-06-11 19:25:18 +0900

    Initial Commit

---
+++ VideoRecorder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VideoRecorder
+//
+//  Created by Young Bin on 2023/06/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoRecorder/VideoRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoRecorderApp.swift
+//  VideoRecorder
+//
+//  Created by Young Bin on 2023/06/11.
+//
+
+import SwiftUI
+
+@main
+struct VideoRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
